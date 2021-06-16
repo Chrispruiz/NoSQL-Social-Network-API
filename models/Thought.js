f712084f5e5ecb88504b0a47e083d1e34a9f02de
Chris Ruiz <chrispruiz@att.net>
2021-06-16 13:23:57 -0500

    added date format

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,5 @@
 const { Schema, model, Types } = require('mongoose');
-const moment = require('moment');
+const dateFormat = require("../utils/dateFormat");
 
 const reactionSchema = new Schema(
     {
@@ -20,7 +20,7 @@ const reactionSchema = new Schema(
       createdAt: {
         type: Date,
         default: Date.now,
-        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        get: (createdAtVal) => dateFormat(createdAtVal),
       }
     },
     {
@@ -41,7 +41,7 @@ const thoughtSchema = new Schema (
         createdAt: {
             type: Date,
             default: Date.now,
-            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+            get: (createdAtVal) => dateFormat(createdAtVal),
         },
         username: {
             type: String,
@@ -63,7 +63,7 @@ const thoughtSchema = new Schema (
 const Thought = model('Thought', thoughtSchema);
 
   // get total count of friends on retrieval
-  ThoughtSchema.virtual('reactionCount').get(function() {
+  thoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
   });
 
